223881ccc5988a18c31aac1d6487c90c23475b77
MFaik <m.faikekclr@gmail.com>
2022-07-16 16:50:44 +0300

    Player temp commit

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -10,8 +10,17 @@ public class BulletManager : MonoBehaviour
     GameObject SpawnLocation;
 
     List<Bullet> bullets = new List<Bullet>();
-    void Start()
-    {
+
+    public static BulletManager Instance { get; private set; }
+    private void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+        } else {
+            Instance = this;
+        }
+    }
+
+    void Start() {
         SpawnBullet(SpawnLocation.transform.position, new Vector2(5, 5), new Vector2(1, 1));
     }
 
@@ -23,11 +32,11 @@ public class BulletManager : MonoBehaviour
         }
     }
 
-    public void SpawnBullet(Vector3 SpawnPosition, Vector2 SpawnIndex, Vector2 velocity) {
-        var bullet = Instantiate(BulletPrefab, SpawnPosition, Quaternion.identity).GetComponent<Bullet>();
+    public static void SpawnBullet(Vector3 SpawnPosition, Vector2 SpawnIndex, Vector2 velocity) {
+        var bullet = Instantiate(Instance.BulletPrefab, SpawnPosition, Quaternion.identity).GetComponent<Bullet>();
 
         bullet.Init(SpawnIndex, velocity);
 
-        bullets.Add(bullet);
+        Instance.bullets.Add(bullet);
     }
 }
