910aa50bc82802ba0c90e2e0b48a54bc0a212f08
Kenpar <erengrl3854@gmail.com>
2022-07-16 13:02:23 +0300

    prototype map

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    GameObject tile;
+    void Start()
+    {
+        GenerateMap(0, 0, 10);
+    }
+
+    void GenerateMap(int x, int y, int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
