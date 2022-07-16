ee1c0c45ffca26fa76a8032de0e138ef6beeb3e4
MFaik <m.faikekclr@gmail.com>
2022-07-16 12:13:56 +0300

    Prefabs and Scripts folder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject RUCorner;
+    [SerializeField] GameObject RDCorner;
+    [SerializeField] GameObject LDCorner;
+    [SerializeField] GameObject LUCorner;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+    }
+
+    IEnumerator PlayerTurn() {
+        
+
+        yield break;
+    }
+}
